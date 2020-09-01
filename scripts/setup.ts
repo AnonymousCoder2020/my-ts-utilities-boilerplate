47517a6936dd44fdfb974e785576d68f0de0fdb1
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-09-01 21:11:44 +0900

    Fix setup script

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -6,11 +6,11 @@ import { promises as fs } from 'fs'
   const fixStringProp = (obj: { [any: string]: string }, key: string) => {
     obj[key] = obj[key].replace(/my-react-ts-boilerplate/, repositoryName)
   }
-  const packageJSON = JSON.parse(await fs.readFile('../package.json', 'utf-8'))
+  const packageJSON = JSON.parse(await fs.readFile('./package.json', 'utf-8'))
   fixStringProp(packageJSON.repository, 'url')
   fixStringProp(packageJSON.bugs, 'url')
   fixStringProp(packageJSON, 'homepage')
-  await fs.writeFile('../package.json', JSON.stringify(packageJSON))
+  await fs.writeFile('./package.json', JSON.stringify(packageJSON, null, '  '))
   exec(
     `git remote set-url origin https://github.com/AnonymousCoder2020/${repositoryName} && git merge origin master --allow-unrelated-histories`,
     (err, stdout, stderr) => {
