5955bcc00c8349855bb6daadf6e9f58d8940064f
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-09-01 20:46:30 +0900

    Add setup script

---
+++ scripts/setup.ts
@@ -0,0 +1,20 @@
+import { exec } from 'child_process'
+import { promises as fs } from 'fs'
+;(async () => {
+  const [, , repositoryName] = process.argv
+  const fixStringProp = (obj: { [any: string]: string }, key: string) => {
+    obj[key] = obj[key].replace(/my-react-ts-boilerplate/, repositoryName)
+  }
+  const packageJSON = JSON.parse(await fs.readFile('../package.json', 'utf-8'))
+  fixStringProp(packageJSON.repository, 'url')
+  fixStringProp(packageJSON.bugs, 'url')
+  fixStringProp(packageJSON, 'homepage')
+  await fs.writeFile('../package.json', JSON.stringify(packageJSON))
+  exec(`git remote set-url origin https://github.com/AnonymousCoder2020/${repositoryName}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log(`(setup.ts error): ${stderr}`)
+      return
+    }
+    console.log(`(setup.ts message): ${stdout}`)
+  })
+})()
