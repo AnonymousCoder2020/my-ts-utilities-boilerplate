215694e3c31b7edbb526689e48855446fe430e51
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-09-18 16:51:08 +0900

    Fix scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { exec } from 'child_process'
 ;(async () => {
-  const [, , scriptName] = process.argv
-  exec(`node --loader ts-node/esm.mjs scripts/${scriptName}.ts`, (err, stdout, stderr) => {
+  const [, , scriptName, ...args] = process.argv
+  exec(`node --loader ts-node/esm.mjs scripts/${scriptName}.ts${args.length ? ' ' + args.join(' ') : ''}`, (err, stdout, stderr) => {
     if (err) {
       console.log(`(script_err ${scriptName}) ${stderr}`)
       return
